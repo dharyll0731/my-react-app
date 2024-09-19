84de6f41bdceb8047481b2ad05aafa05ee3a3a3b
unknown <floresdharyll0731@gmail.com>
2024-09-19 19:54:29 +0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
-});
+  base:"/my-react-app",
+  plugins: [react()],
+})
